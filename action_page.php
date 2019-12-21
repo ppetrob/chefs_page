d612113c9e0a003f583a5a78b7506607881cec78
ppetrob <56272210+ppetrob@users.noreply.github.com>
2019-12-21 07:37:59 +0000

    new php
    new php for sending emails via contact us

--- action_page.php
+++ action_page.php
@@ -1,12 +1,12 @@
 <?php
 if(isset($_POST['submit'])){
     $to = "ppetrob@gmail.com.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
+    $from = $_POST['lastname']; // this is the sender's Email address
+    $first_name = $_POST['firstname'];
+    // $last_name = $_POST['last_name'];
     $subject = "Form submission";
     $subject2 = "Copy of your form submission";
-    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message = $first_name . " wrote the following:" . "\n\n" . $_POST['message'];
     $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
 
     $headers = "From:" . $from;
